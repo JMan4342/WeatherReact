5b3444254a81808a6f776d44cd7970e1be38baf9
Joseph Manning <jgmanning3@yahoo.com>
2021-12-12 13:47:29 -0700

    Create route for current weather

---
+++ src/Components/CurrentWeather.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function CurrentWeather(props) {
+    return (
+        <div>
+            <div>
+                <h2> Current Weather</h2>
+            </div>
+        </div>
+    )
+}
